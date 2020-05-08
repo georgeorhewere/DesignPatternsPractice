66d9f7e3233f8d1464316b7bd01d8b9594ee2b1a
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-05-08 22:11:51 +0100

    Added director and reference in main project to test

---
+++ BuilderPattern/ComputerDirector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class ComputerDirector
+    {
+        
+
+
+        public void Create()
+        {
+            ComputerBuilder builder = new Laptop($"HP");
+            builder.AddHardDisk();
+            builder.AddRAM();
+            builder.AddProcessor();
+            builder.GetSpecifications();
+            Console.WriteLine("-----------");
+            
+            builder = new Desktop("Dell");
+            builder.AddHardDisk();
+            builder.AddRAM();
+            builder.AddProcessor();
+            builder.GetSpecifications();
+            Console.WriteLine("-----------");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuilderPattern;
+using System;
 
 namespace DesignPatternsPractice
 {
@@ -6,7 +7,9 @@ namespace DesignPatternsPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Practicing Design Patterns");
+            ComputerDirector director = new ComputerDirector();
+
         }
     }
 }
