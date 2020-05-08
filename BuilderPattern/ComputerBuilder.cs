c94c8d2885fc6409536b9d12f253af739beac1ea
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-05-08 21:47:12 +0100

    created abstract builder for computers and the computer product returned

---
+++ BuilderPattern/ComputerBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    public abstract class ComputerBuilder
+    {
+        protected string model;
+        private ComputerProduct product;
+
+        public ComputerBuilder(string _model)
+        {
+            model = _model;
+            product = new ComputerProduct();
+        }
+        public abstract void AddHardDisk();
+        public abstract void AddRAM();
+        public abstract void AddProcessor();
+        public ComputerProduct GetProduct() {
+            return product;
+        }
+
+    }
+}
---
+++ BuilderPattern/ComputerProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class ComputerProduct
+    {
+
+        private List<string> Specifications;
+        public ComputerProduct()
+        {
+            Specifications = new List<string>();
+        }
+
+        public void Add(string spec)
+        {
+            Specifications.Add(spec);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Specifications ");
+            foreach (var item in Specifications)
+            {
+                Console.WriteLine($"{Specifications.IndexOf(item) +1 }. {item}");
+            }
+        }
+
+
+    }
+}
